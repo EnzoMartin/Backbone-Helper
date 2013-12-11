e21a55bb71d06b5d7cdc9b2029e68ef4bd5e329b
Enzo Martin <enzo.r.martin@gmail.com>
2013-12-11 16:29:49 +0100

    Don't overwrite the Backbone object.. - Bump version to 1.0.2

--- backbone-helper.js
+++ backbone-helper.js
@@ -1,7 +1,7 @@
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
         define(['backbone', 'underscore', 'exports'], function (Backbone, _, exports) {
-            root.Backbone = factory(root, exports, Backbone, _);
+            root.BB = factory(root, exports, Backbone, _);
         });
     } else if (typeof exports !== 'undefined') {
         var Backbone = require('backbone');
