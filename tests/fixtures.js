cb46cd45658e76a1de7daced5bde5703ca147a8f
Enzo Martin <EnzoMartin78@users.noreply.github.com>
2014-05-07 12:32:56 +0200

    Add new fixtures

--- tests/fixtures.js
+++ tests/fixtures.js
@@ -2,10 +2,16 @@ var Backbone = require('backbone');
 var BB = require('../backbone-helper');
 
 BB.model_definitions.person = Backbone.Model.extend({});
+BB.model_definitions.ship = Backbone.Model.extend({});
 BB.model_definitions.car = Backbone.Model.extend({});
 BB.collection_definitions.cars = Backbone.Collection.extend({
     model: function (attributes,options) {
         return new BB.model_definitions.car(attributes,options);
+    }
+});
+BB.collection_definitions.ships = Backbone.Collection.extend({
+    model: function (attributes,options) {
+        return new BB.model_definitions.ship(attributes,options);
     }
 });
 BB.collection_definitions.persons = Backbone.Collection.extend({
